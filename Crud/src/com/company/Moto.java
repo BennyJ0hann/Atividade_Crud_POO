d4de0591ce39d2af1577f1f21b5151317116e0b0
BennyJ0hann <benny.oliveira@cromg.org.br>
2024-04-03 15:41:46 -0300

    ci: Classe Alterada

--- Crud/src/com/company/Moto.java
+++ Crud/src/com/company/Moto.java
@@ -1,3 +1,40 @@
 public class Moto {
 
+    public String modelo;
+    public String marca;
+    public int ano;
+    public String cor;
+    public String chassi;
+    public int revisoes;
+    public double km;
+    
+    public Moto(String modelo, String marca, int ano, String cor, String chassi){
+        this.modelo = modelo;
+        this.marca = marca;
+        this.ano = ano;
+        this.cor = cor;
+        this.chassi = chassi;
+
+        this.revisoes = 0;
+        this.km = 0;
+
+    }
+    public String getModelo(String modelo){
+        return this.modelo;
+    }
+    public String getMarca(String marca){
+        return this.marca;
+    }
+    public int getAno(String ano){
+        return this.ano;
+    }
+    public String getCor(String cor){
+        return this.cor;
+    }
+    public String getChasi(String chassi){
+        return this.chassi;
+    }
+    public String getNome(String chassi){
+        return this.chassi;
+    }
 }
